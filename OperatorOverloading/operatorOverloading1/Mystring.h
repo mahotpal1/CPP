685946e5dcdf92308e4d25a4f88fd39bdf6ad591
mahotpal1 <mahotpalvishal87@gmail.com>
2022-12-01 23:30:13 +0530

    added operator overloading

---
+++ OperatorOverloading/operatorOverloading1/Mystring.h
@@ -0,0 +1,15 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+class Mystring{
+  private :
+    char *str; // c-style string
+  public :
+    Mystring();
+    Mystring(const char *s);
+    Mystring(const Mystring &source);
+    ~Mystring();
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+};
+#endif
